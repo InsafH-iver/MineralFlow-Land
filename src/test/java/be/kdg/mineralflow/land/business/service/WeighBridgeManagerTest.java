ff97a518ecdbb24e4a7acefaa51b0644051cdc36
H-iver <insaf.hichour@student.kdg.be>
2024-09-30 00:10:21 +0200

    test aangepast

--- src/test/java/be/kdg/mineralflow/land/business/service/WeighBridgeManagerTest.java
+++ src/test/java/be/kdg/mineralflow/land/business/service/WeighBridgeManagerTest.java
@@ -5,6 +5,10 @@ import be.kdg.mineralflow.land.config.ConfigLoader;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 import static be.kdg.mineralflow.land.config.ConfigProperties.WEIGHBRIDGE_AMOUNT;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,6 +18,20 @@ class WeighBridgeManagerTest {
 
     @Autowired
     private WeighBridgeManager weighBridgeManager;
+
+    static PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"))
+                    .withDatabaseName("testdb")
+                    .withUsername("test")
+                    .withPassword("test");
+
+    @DynamicPropertySource
+    static void registerPgProperties(DynamicPropertyRegistry registry) {
+        postgreSQLContainer.start();
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    }
 
     @Test
     void getWeighBridgeNumber() {
